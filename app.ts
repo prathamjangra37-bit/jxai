d2e5b5c692f4c4b3567633c104a20656a6fe8c61
prathamjangra37-bit <prathamjangra37@gmail.com>
2026-07-09 14:09:07 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -5,12 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
-import express from "express";
-import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const app = express();
 
