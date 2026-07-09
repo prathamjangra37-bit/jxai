cde019f775f07b73f93a5c62682a27025a6d5f16
prathamjangra37-bit <prathamjangra37@gmail.com>
2026-07-09 14:18:57 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,7 +2,9 @@
 import express from "express";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv"
+dotenv.config();
 
+const app = express();
 // Set body parsers...
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
